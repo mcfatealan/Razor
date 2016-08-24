ae609e67540db91b32a6af4244e2ae2e4cfdd738
Mcfatealan <louchang_new@163.com>
2016-08-24 10:48:17 +0800

    test

--- razor.js
+++ razor.js
@@ -1,6 +1,19 @@
 new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue.js!'
+        message: 'Hello Vue.js!',
+        csv_file_content: ''
+    },
+    methods: {
+    groupMethod: function () {
+        var csvContent = 'xx';
+        var encodedUri = encodeURI(csvContent);
+        var link = document.createElement("a");
+        link.setAttribute("href", encodedUri);
+        link.setAttribute("download", "my_data.csv");
+        document.body.appendChild(link); // Required for FF
+
+        link.click();
     }
+  }
 })
