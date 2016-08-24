402ae94be9c7ddfbcd69d43fc466c804df55a8cc
Mcfatealan <louchang_new@163.com>
2016-08-24 20:41:45 +0800

    finish

--- razor.js
+++ razor.js
@@ -55,7 +55,10 @@ new Vue({
     data: {
         group_num_string: null,
         group_num: null,
-        studentPool: []
+        studentPool: {'malePool':[],'femalePool':[]},
+        groups:[],
+
+        staticsMessage: ''
     },
     methods: {
         passTest: function () {
@@ -74,15 +77,26 @@ new Vue({
                 
                 var newStudent = {
                     'id': student,
-                    'sex': true,
                     'score': 0,
                     'info': inputdata[student],
                 };
                 
+                
+                newStudent.score = Number(inputdata[student].score);
+                if(isNaN(newStudent.score) || inputdata[student].score=='')
+                {
+                    var alertText='';
+                    alertText += '第'+(parseInt(student)+1).toString()+'行数据出错:\n';
+                    alertText += JSON.stringify(inputdata[student])+'\n';
+                    alertText += '出错原因： 分数必须合理';
+                    alert(alertText);
+                    return false;
+                }
+
                 if(inputdata[student].sex=='男')
-                    newStudent.sex = true;
+                    this.studentPool.malePool.push(newStudent);
                 else if(inputdata[student].sex=='女')
-                    newStudent.sex = false;
+                    this.studentPool.femalePool.push(newStudent);
                 else
                 {
                     var alertText='';
@@ -92,30 +106,124 @@ new Vue({
                     alert(alertText);
                     return false;
                 }
-
-                newStudent.score = Number(inputdata[student].score);
-                if(isNaN(newStudent.score))
-                {
-                    var alertText='';
-                    alertText += '第'+(parseInt(student)+1).toString()+'行数据出错:\n';
-                    alertText += JSON.stringify(inputdata[student])+'\n';
-                    alertText += '出错原因： 分数必须合理';
-                    alert(alertText);
-                    return false;
-                }
     
-                studentPool.append(newStudent);
             }
 
             return true;
         },
+        subgroupAlgo: function (pool,sex) {
+            pool.sort(function(a, b){
+                var keyA = a.score,
+                    keyB = b.score;
+                if(keyA > keyB) return -1;
+                if(keyA < keyB) return 1;
+                return 0;
+            });
+    
+            while(pool.length>0)
+            {
+                var theStudent;
+                
+                var remainGroups = Array.apply(null, Array(this.group_num)).map(function (_, i) {return i;});
+                while(remainGroups.length>0)
+                {
+                    if(pool.length==0)
+                        break;
+
+                    theStudent = pool[0];
+
+                    var randomIndex = Math.floor(Math.random() * remainGroups.length);
+                    var groupID = remainGroups[randomIndex];
+                    console.log(randomIndex);
+                    console.log(groupID);
+                    this.groups[groupID].pool.push(theStudent);
+                    if(sex)
+                        this.groups[groupID].maleCount += 1;
+                    else
+                        this.groups[groupID].femaleCount += 1;
+
+                    this.groups[groupID].max = Math.max(this.groups[groupID].max, theStudent.score);
+                    this.groups[groupID].min = Math.min(this.groups[groupID].min, theStudent.score);
+                    this.groups[groupID].totalScore += theStudent.score;
+                    
+                    remainGroups.splice(randomIndex, 1);
+                    pool.splice(0, 1);
+                }
+            }
+
+        },
+
         groupAlgo: function () {
-            outputdata = JSON.parse(JSON.stringify(inputdata));
+            for(var groupIndex=0; groupIndex<this.group_num;groupIndex++)
+            {
+                this.groups.push({
+                    'pool': [],
+                    'maleCount': 0,
+                    'femaleCount': 0,
+                    'max': -99999,
+                    'min': 99999,
+                    'totalScore': 0,
+                });
+            }
+
+            this.subgroupAlgo(this.studentPool.malePool,true);
+            this.subgroupAlgo(this.studentPool.femalePool,false);
+
+            outputdata = [];
+            for(var groupIndex=0; groupIndex<this.group_num;groupIndex++)
+            {
+                var grouppool = this.groups[groupIndex].pool;
+                for(elem in grouppool)
+                {
+                    var info = grouppool[elem].info;
+                    info.group = groupIndex+1;
+                    outputdata.push(info);
+                }
+            }
+            
             table_output.loadData(outputdata);
         },
+        printStatics: function (ifsuccess) {
+            if(!ifsuccess)
+            {
+                this.message = '失败';
+                this.staticsMessage = '输入错误，生成失败';
+            }
+            else
+            {
+                this.message = '成功';
+                this.staticsMessage = '';
+                for(var groupIndex=0; groupIndex<this.group_num;groupIndex++)
+                {
+
+                    this.staticsMessage += '第'+(Number(groupIndex)+1).toString()+'班情况:<br />';
+                    this.staticsMessage += '&nbsp; 男女人数:'+ this.groups[groupIndex].maleCount+':'+this.groups[groupIndex].femaleCount+' <br />';
+                    this.staticsMessage += '&nbsp; 最高分:'+this.groups[groupIndex].max+' <br />';
+                    this.staticsMessage += '&nbsp; 最低分:'+this.groups[groupIndex].min+' <br />';
+                    this.staticsMessage += '&nbsp; 平均分:'+this.groups[groupIndex].totalScore/(this.groups[groupIndex].maleCount+this.groups[groupIndex].femaleCount)+'<br />';
+                    this.staticsMessage += '<br />';
+                }
+            }            
+        },
+        init: function () {
+            this.studentPool = {'malePool':[],'femalePool':[]};
+            this.groups = [];
+
+            this.staticsMessage = '';
+
+        },
+
         main: function () {
+            this.init();
+
             if(this.passTest())
+            {
                 this.groupAlgo();
+                this.printStatics(true);
+            }
+            else
+                this.printStatics(false);
+
         }
     }
 })
